63832ad7745d8e582567370ec8ff0e7cfe091abc
Pablo Diehl <pablodiehl@ymail.com>
2024-12-19 11:01:00 -0300

    feat(templates): new template vuepress-hope-docs

---
+++ templates/vuepress/vuepress-hope-docs/docs/.vuepress/config.ts
@@ -0,0 +1,23 @@
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Docs Demo",
+      description: "A docs demo for vuepress-theme-hope",
+    }
+  },
+
+  bundler: viteBundler(),
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
