2648aa5789e5aa75a35cca56cb217808d0f38805
Pablo Diehl <pablodiehl@ymail.com>
2024-12-19 10:11:46 -0300

    fix(vuepress-hope-blog): remove multi-language

--- templates/vuepress/vuepress-hope-blog/docs/.vuepress/config.ts
+++ templates/vuepress/vuepress-hope-blog/docs/.vuepress/config.ts
@@ -14,12 +14,12 @@ export default defineUserConfig({
       lang: "en-US",
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
-    },
+    }//,
+    //"/zh/": {
+    //  lang: "zh-CN",
+    //  title: "博客演示",
+    //  description: "vuepress-theme-hope 的博客演示",
+    //},
   },
 
   bundler: viteBundler(),
